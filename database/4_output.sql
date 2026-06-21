2555201df30b67b7c1044ca3da0bea85e894c551
MarianoChun <marianochun01@gmail.com>
2026-06-20 23:43:28 -0300

    renaming

--- database/4_output.sql
+++ database/4_output.sql
@@ -1,20 +1,18 @@
 -- Print GLC en FNC por consola
 CREATE OR REPLACE FUNCTION print_glc_fnc() RETURNS VOID AS $$
 DECLARE
-    registro record;
+    variable record;
     glc_output text := CHR(10) || '';
 BEGIN
-    -- Una sola consulta limpia que hace todo el trabajo
-    FOR registro IN 
+    FOR variable IN 
         SELECT 
             parte_izq,
-            string_agg(CONCAT(parte_der1, ' ', parte_der2), ' | ') AS opciones
+            string_agg(CONCAT(parte_der1, ' ', parte_der2), ' | ') AS partes_der
         FROM GLC_EN_FNC
         GROUP BY parte_izq, start, tipo_produccion
         ORDER BY start DESC, tipo_produccion DESC, parte_izq DESC
     LOOP
-        -- El cuerpo del bucle queda sumamente legible
-        glc_output := glc_output || registro.parte_izq || ' --> ' || registro.opciones || CHR(10);
+        glc_output := glc_output || variable.parte_izq || ' --> ' || variable.partes_der || CHR(10);
     END LOOP;
     
     RAISE NOTICE '%', glc_output;
