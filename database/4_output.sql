a5ab7386f88c92ffdf954dcfeee3e416082e310a
MarianoChun <marianochun01@gmail.com>
2026-06-21 00:23:00 -0300

    Add print matriz cyk

--- database/4_output.sql
+++ database/4_output.sql
@@ -19,4 +19,38 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT print_glc_fnc();
+-- Print matriz CYK
+ORDER BY i ASC, j ASC;
+
+CREATE OR REPLACE FUNCTION print_matriz_cyk() RETURNS VOID AS $$
+DECLARE
+ fila_i int;
+ fila_j int;
+ fila int;
+ size_matriz int;
+ matriz_output text := CHR(10) || '';
+ variables_celda_matriz text[];
+BEGIN
+	SELECT MAX(j) into size_matriz FROM matriz_cyk;
+	FOR fila IN REVERSE size_matriz..1 LOOP
+		IF fila = 1 THEN
+			FOR fila_i in 1..size_matriz LOOP
+				SELECT x INTO variables_celda_matriz FROM matriz_cyk WHERE i = fila_i;
+				matriz_output := matriz_output || 'X' || fila_i || fila_i || '={' || array_to_string(variables_celda_matriz, ',') || '} ';
+			END LOOP;
+		ELSE
+			FOR fila_i IN 1..(size_matriz - fila + 1) LOOP
+            	fila_j := fila_i + fila - 1;
+				SELECT x INTO variables_celda_matriz FROM matriz_cyk WHERE i = fila_i and j = fila_j;
+				matriz_output := matriz_output || 'X' || fila_i || fila_j || '={' || array_to_string(variables_celda_matriz, ',') || '} ';
+			END LOOP;
+		END IF;
+		matriz_output := matriz_output || CHR(10);
+	END LOOP;
+	RAISE NOTICE '%', matriz_output;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
